ad8b7a2c14e5daa66c977a29ba0f98563715e966
Nizi95 <nlamouri@mbp-de-lamouri.home>
2020-10-14 15:48:58 +0200

    Doc and feat(index.js, readme.md): Creating the progress bar

---
+++ exercice-1/index.js
@@ -0,0 +1,44 @@
+'use strict';
+/*
+*gridGenerator
+*@param {number} xAxis
+*@param {number} yAxis
+*/
+var gridGenerator = function gridGenerator(xAxis, yAxis){
+    this.xAxis = xAxis;
+    this.yAxis = yAxis;
+}
+
+gridGenerator.prototype.renderTable = function(){
+    var body = document.querySelector('body');
+    var table = document.createElement('table');
+    for(var i = 1; i <= this.yAxis; i++){
+        var tr = document.createElement('tr');
+        for(var j = 1; j <= this.xAxis; j++){
+            var td = document.createElement('td');
+            var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+            td.style.backgroundColor = randomColor;
+            td.style.width = '70px';
+            td.style.height = '50px';
+            tr.appendChild(td);
+        }table.appendChild(tr);
+    }body.appendChild(table);
+}
+
+gridGenerator.prototype.run = function(){
+    this.renderTable();
+    setInterval(function(){
+      this.randomColor()
+      }.bind(this), 2000);
+}
+
+gridGenerator.prototype.randomColor = function(){
+    var td_list = document.querySelectorAll('td');
+    td_list.forEach(function(td_list){
+      var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+      td_list.style.backgroundColor = randomColor;
+    });
+}
+
+var tab = new gridGenerator(12, 12);
+tab.run();
---
+++ exercice-1/readme.md
@@ -0,0 +1,13 @@
+# Evaluation JS
+
+## [](https://github.com/Nizi95/Demo-1#context)Context
+
+Créer une fonction “gridGenerator” prenant les arguments “xAxis” et “yAxis”. Vous devez générer une matrice en “table” html de x * y. Vous devrez colorer aléatoirement chaque case de la grille d’une couleur différente toutes les “1s” à “2s”.
+
+### [](https://github.com/Nizi95/Evaluation-JS.git#tech)Tech
+
+Technology used: *HTML  *JS
+
+### [](https://github.com/Nizi95/Evaluation-JS.git#author)Author
+
+[**LAMOURI Nizar**](https://github.com/Nizi95) 
